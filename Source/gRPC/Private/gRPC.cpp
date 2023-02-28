20bc3c7f1208e40ef65254b94689a6c7ffef48b8
Kevin Boon <kevin_boon@outlook.com>
2023-02-28 18:56:29 -0500

    Added gRPC Game Settings

--- Source/gRPC/Private/gRPC.cpp
+++ Source/gRPC/Private/gRPC.cpp
@@ -3,6 +3,12 @@
 #include "gRPC.h"
 #include "Modules/ModuleManager.h"
 
+#if WITH_EDITOR
+#include "ISettingsModule.h"
+#include "ISettingsSection.h"
+#include "gRPCSettings.h"
+#endif
+
 
 #define LOCTEXT_NAMESPACE "FgRPCModule"
 
@@ -11,11 +17,31 @@ DEFINE_LOG_CATEGORY(gRPC);
 void FgRPCModule::StartupModule()
 {
 	UE_LOG(gRPC, Log, TEXT("Startup gRPC Module"));
+
+#if WITH_EDITOR
+	ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings");
+	if (SettingsModule != nullptr)
+	{
+		SettingsModule->RegisterSettings("Project", "Game", "gRPC",
+			LOCTEXT("gRPCSettingsName", "gRPC"),
+			LOCTEXT("gRPCSettingsDescription", "Configure gRPC Network Service"),
+			GetMutableDefault<UgRPCSettings>()
+		);
+	}
+#endif
 }
 
 void FgRPCModule::ShutdownModule()
 {
 	UE_LOG(gRPC, Log, TEXT("Shutdown gRPC Module"));
+
+#if WITH_EDITOR
+	ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings");
+	if (SettingsModule)
+	{
+		SettingsModule->UnregisterSettings("Project", "Game", "gRPC");
+	}
+#endif
 }
 
 #undef LOCTEXT_NAMESPACE
---
+++ Source/gRPC/Public/gRPCSettings.h
@@ -0,0 +1,15 @@
+// Copyright Codeware Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "gRPCSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config = Game, defaultconfig, meta = (DisplayName = "gRPC"))
+class GRPC_API UgRPCSettings : public UObject
+{
+	GENERATED_BODY()
+};
