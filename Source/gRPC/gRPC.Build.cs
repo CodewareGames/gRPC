f6c0eb29775ac1688139d28a3a970fca15f53b62
Kevin Boon <kevin_boon@outlook.com>
2023-02-28 23:13:44 -0500

    Added CMake Structure

--- Source/gRPC/gRPC.Build.cs
+++ Source/gRPC/gRPC.Build.cs
@@ -1,32 +1,22 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class gRPC : ModuleRules
 {
+	protected readonly string Version = "";
+
 	public gRPC(ReadOnlyTargetRules Target) : base(Target)
 	{
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
 			}
 			);
 			
@@ -38,16 +28,30 @@ public class gRPC : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
 			}
 			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "OpenSSL");
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "zlib");
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "Protobuf");
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "Re2");
+
+		string VersionPath = Path.Combine(ModuleDirectory, Version);
+		string LibraryPath = Path.Combine(VersionPath, "lib");
+
+		PublicSystemIncludePaths.Add(Path.Combine(VersionPath, "include"));
+
+		if (Target.IsInPlatformGroup(UnrealPlatformGroup.Unix))
+		{
+
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+
+		}
 	}
 }
